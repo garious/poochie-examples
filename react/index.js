6a2d1f80ecc3b9a181e5ca01961bb6330715cbe4
Greg Fitzgerald <garious@gmail.com>
2015-03-27 11:52:48 -0700

    gizmo, great word

--- react/index.js
+++ react/index.js
@@ -1,5 +1,5 @@
 var deps = [
-    'widgets.js',
+    'gizmos.js',
     '/stdlib/observable.js'
 ];
 
@@ -7,12 +7,12 @@ function increment(x) {
     return String(parseInt(x, 10) + 1);
 }
 
-function onReady(widgets, observable) {
+function onReady(gizmos, observable) {
     var o = observable.publisher("0");
     var oIncrement = observable.lift(increment);
-    var box = widgets.box([
-        widgets.numInput(o),
-        widgets.numOutput(oIncrement(o))
+    var box = gizmos.box([
+        gizmos.numInput(o),
+        gizmos.numOutput(oIncrement(o))
     ]);
     define(box);
 }
